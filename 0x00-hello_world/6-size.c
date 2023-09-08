23522fce5dbbb426cf871e7a9866b035d1e8b88d
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-07 21:54:12 -0700

    added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that print the size of a type
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long log int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byted(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
