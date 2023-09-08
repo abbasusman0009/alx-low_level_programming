c8e65593a89bcfc802e52872e54c03bdc9eaa10f
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-07 20:09:22 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that print aa line with put function
  * return 0 success
  */
-int main(void)
+int main(void) /* is the entry point that everything is execute */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
