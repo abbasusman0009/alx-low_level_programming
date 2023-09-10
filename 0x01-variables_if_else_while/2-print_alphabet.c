beec610a1bc74eb0a79af319f1c6557b69818198
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-10 15:59:30 -0700

    added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - A program that print alphabet in lowercase letters
+  * Return: 0(Success)
+  */
+  int main(void)
+  {
+	char x;
+	x = 'a';
+
+	while(x <= 'z')
+	{
+		putchar(x);
+		putchar('\n');
+		x = x + 1;
+	}
+	return (0);
+  }
