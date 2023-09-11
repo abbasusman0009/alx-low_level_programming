049be9c9072d9b5eb64990add19a8ca9df886ab7
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-10 21:44:41 -0700

    added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - A program that will print alphabate from a-z without e and q
+  * Return: 0(Success)
+  */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+
+	while (x <= 'z' && !(x == 'e' || x == 'q'))
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
