e349f32abb49e0021e48701999c9807edbae56ee
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-10 22:15:37 -0700

    added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - A program to o print 0-9
+  * Return: 0(Succes)
+  */
+int main(void)
+{
+	int x;
+
+	x = '0';
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
