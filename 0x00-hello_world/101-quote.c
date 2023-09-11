abd877b230db3648fa8b79a0696f4760ef9a7c53
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-11 07:24:44 -0700

    added text file in c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - A program that print a text
+  * Return: 1(Succes)
+  */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
