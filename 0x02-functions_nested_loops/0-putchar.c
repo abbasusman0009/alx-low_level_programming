5e254aac1237e3b4ed3b8f35a6f68f7474453af4
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-13 16:27:44 -0700

    added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main- A progrma that print _putchar
+ * Return: 0(Succes)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
