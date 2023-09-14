c74543c63b0c49cd50b2b215ca41affe0f2c0f7a
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-13 21:40:33 -0700

    added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - check for lower case letter
+ * @x! the character to be cheked
+ * Return: 1 for lowwer case letter or 0 for anything else
+ */
+
+int _islower(int x)
+{
+	if (x >= 97 && x <= 122)
+	{
+	return (1);
+	}
+	return (1);
+}
