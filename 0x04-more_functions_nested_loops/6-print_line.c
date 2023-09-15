d6efdd85d581f6c6f687dc91c5fe4b44e538ead7
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-15 16:04:35 -0700

    addded

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * print_line - A progrma that print a straight line
+  * Return: 0
+  *@n: a number
+  */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x;
+
+		x = 1;
+		while (x <= n)
+		{
+			_putchar(x);
+			x++;
+		}
+		_putchar('\n');
+	}
+}
