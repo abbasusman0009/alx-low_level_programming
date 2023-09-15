1d4a076e1f471bdc2792469f4cc1953c90f0a6c5
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-15 09:39:42 -0700

    added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - A program that return a*b
+ * @a: that store a 1 value
+ * @b: that store b 2 value
+ * Return: Mul of a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
