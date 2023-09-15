1573a828d71e07ce393168ebc5e52c04db487899
Abbas Usman Idris <abbasusman0009@gmail.com>
2023-09-15 09:54:12 -0700

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - A program that display 0-9
+ * Return 0
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		_putchar(x);
+	_putchar('\n');
+}
